19760870dc56f7fd4905a5e953d37ee0c0f63c08
GOG777 <geniusgenius877@gmail.com>
2025-12-03 01:59:42 +0100

    Changed Galaxy Icon on review

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -446,7 +446,7 @@ const HomePage: React.FC = () => {
                         </h3>
                         <p className="text-lg text-purple-400 font-semibold">{project.type}</p>
                       </div>
-                      <span className="px-4 py-2 bg-green-500/20 border border-green-500/50 rounded-full text-sm text-green-300">
+                      <span className="px-4 py-2 bg-green-500/20 border border-green-500/50 rounded-full text-sm text-green-300 whitespace-nowrap text-center inline-block min-w-[180px]">
                         {project.status}
                       </span>
                     </div>
